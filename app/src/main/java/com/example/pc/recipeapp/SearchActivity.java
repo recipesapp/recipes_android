10b8b7a799f451ed56fbf0651a1aa308953bcc0d
Melani <melchetyyy@gmail.com>
2016-07-20 14:50:11 +0300

    My Profile is almost ready

--- app/src/main/java/com/example/pc/recipeapp/SearchActivity.java
+++ app/src/main/java/com/example/pc/recipeapp/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.pc.recipeapp;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,10 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 
@@ -25,6 +30,11 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     private ArrayList<Recipe> mRecipies;
     private ArrayAdapter<String> mItemsAdapter;
     private boolean responseReceived;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +57,12 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
         //TODO: uncomment and add webservice url when it is known
 //        getData();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void getData(){
+    private void getData() {
         Service.getApiService().getRecepies(new Callback<ArrayList<Recipe>>() {
             @Override
             public void success(ArrayList<Recipe> recipes, Response response) {
@@ -69,10 +82,10 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_search){
-            if (responseReceived){
+        if (v.getId() == R.id.btn_search) {
+            if (responseReceived) {
                 String searchWord = mRecipiName.getText().toString();
-                if (!searchWord.equals("")){
+                if (!searchWord.equals("")) {
                     mListData.clear();
                     mListData.addAll(sserachInRecepyArray(searchWord));
                     mItemsAdapter.notifyDataSetChanged();
@@ -87,7 +100,7 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    private ArrayList<String> sserachInRecepyArray(String keyWord){
+    private ArrayList<String> sserachInRecepyArray(String keyWord) {
         ArrayList<String> result = new ArrayList<>();
         for (Recipe r : mRecipies) {
             if (r.contains(keyWord))
@@ -95,5 +108,45 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         }
 
         return result;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Search Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.pc.recipeapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Search Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.pc.recipeapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
