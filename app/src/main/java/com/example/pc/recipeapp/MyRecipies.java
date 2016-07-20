8d562487397211a635cfdb675a13310d8914f19e
Melani <melchetyyy@gmail.com>
2016-07-20 17:11:43 +0300

    My Profile is almost ready Images

---
+++ app/src/main/java/com/example/pc/recipeapp/MyRecipies.java
@@ -0,0 +1,13 @@
+package com.example.pc.recipeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyRecipies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recipies);
+    }
+}
